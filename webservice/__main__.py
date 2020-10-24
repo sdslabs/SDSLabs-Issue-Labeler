8315dd37f2a7fe6514896fa5f992b3801f68386b
mhk19 <mahak_g@cs.iitr.ac.in>
2020-10-24 18:25:26 +0530

    add function to get labels and refactor code

--- webservice/__main__.py
+++ webservice/__main__.py
@@ -1,9 +1,20 @@
 import os
+from os import error
 import aiohttp
 from aiohttp import web
-from aiohttp.client import request
 from gidgethub import routing, sansio
 from gidgethub import aiohttp as gh_aiohttp
+from webservice.labels import get_labels
+import yaml
+
+CUR_DIR = os.path.dirname(os.path.abspath(__file__))
+yamlFilePath = CUR_DIR + "/config.yml"
+try:
+    confFile = open(yamlFilePath, "r")
+except FileNotFoundError:
+    print("The required config file " + yamlFilePath + " does not exist")
+    exit(1)
+ENV_VARS = yaml.safe_load(confFile)
 
 router = routing.Router()
 
@@ -11,26 +22,32 @@ routes = web.RouteTableDef()
 
 @router.register("issues", action="opened")
 async def issue_opened_event(event, gh, *args, **kwargs):
-    url = event.data["issue"]["url"]+"/labels"
-    label = "test"
-    await gh.post(url, data={"labels":[label]})
+    issue = event.data["issue"]
+    url = issue["url"]+"/labels"
+    title = issue["title"]
+    body = issue["body"]
+    labels = get_labels(title, body)
+    await gh.post(url, data={"labels": labels})
 
 @routes.post("/")
 async def main(request):
     body = await request.read()
-    secret = os.environ.get("GH_SECRET")
-    oauth_token = os.environ.get("GH_AUTH")
+    secret = ENV_VARS['secret_key']
+    oauth_token = ENV_VARS['oauth_token']
+    username = ENV_VARS['username']
     event = sansio.Event.from_http(request.headers, body, secret=secret)
     async with aiohttp.ClientSession() as session:
-        gh = gh_aiohttp.GitHubAPI(session, "mhk19", oauth_token=oauth_token)
+        gh = gh_aiohttp.GitHubAPI(session, username, oauth_token=oauth_token)
         await router.dispatch(event, gh)
     return web.Response(status=200)
 
 if __name__ == "__main__":
     app = web.Application()
     app.add_routes(routes)
-    port = os.environ.get("PORT")
+    port = ENV_VARS['port']
     if port is not None:
         port = int(port)
+    else:
+        print("Port not found")
     
     web.run_app(app, port=port)
